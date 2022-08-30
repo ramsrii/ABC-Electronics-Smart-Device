678ad83bec5051ebaab46f03e770f68c3ddbe7fd
ramsrii <sriram16nov@gmail.com>
2022-08-30 10:02:18 +0530

    First commit

---
+++ ABC-Electronics-SmartDevice/src/main/java/com/capgemini/SmartDevice/AbcElectronicsSmartDeviceApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.SmartDevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcElectronicsSmartDeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcElectronicsSmartDeviceApplication.class, args);
+		System.out.println("checking....");
+	}
+
+}
---
+++ ABC-Electronics-SmartDevice/src/test/java/com/capgemini/SmartDevice/AbcElectronicsSmartDeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.SmartDevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcElectronicsSmartDeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
