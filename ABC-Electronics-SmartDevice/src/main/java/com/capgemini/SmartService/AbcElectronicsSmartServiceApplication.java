ead947062cde0a01616376883d521248b96ed051
Rodge3 <muthumohan0305@gmail.com>
2022-09-01 16:36:33 +0530

    check2

--- ABC-Electronics-SmartDevice/src/main/java/com/capgemini/SmartService/AbcElectronicsSmartServiceApplication.java
+++ ABC-Electronics-SmartDevice/src/main/java/com/capgemini/SmartService/AbcElectronicsSmartServiceApplication.java
@@ -8,7 +8,7 @@ public class AbcElectronicsSmartServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbcElectronicsSmartServiceApplication.class, args);
-		System.out.println("checking..");
+		System.out.println("checking.. one ");
 	}
 
 }
